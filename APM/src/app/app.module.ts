d70898933d956eb71f88da6efb5a58b5e4675c40
Rella <anthonyrella@outlook.com>
2019-03-06 16:50:39 -0500

    Services and Dependency Injection

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductService } from './products/product.service';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { StarComponent } from './shared/star.component';
     BrowserModule, 
     FormsModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], 
+  providers: [ProductService]
 })
 export class AppModule { }
--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -23,35 +24,12 @@ export class ProductListComponent implements OnInit{
 
     filteredProducts: IProduct[];
 
-    products: IProduct[] = [
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-          },
-          {
-            "productId": 8,
-            "productName": "Saw",
-            "productCode": "TBX-0022",
-            "releaseDate": "May 15, 2016",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 3.7,
-            'imageUrl': "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-          }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private productService: ProductService) {
     }
 
-    onRatingClicked(message:string): void {
+    onRatingClicked(message: string): void {
         this.pageTitle = 'Product List: ' + message;
     }
 
@@ -66,6 +44,7 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        console.log('In onInit');
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 }
