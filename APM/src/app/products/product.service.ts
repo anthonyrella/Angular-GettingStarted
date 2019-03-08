f85e4bcd2ab917e4cb37887b24ed6a489b733587
Rella <anthonyrella@outlook.com>
2019-03-08 15:44:31 -0500

    Product Detail Template, Component and Product Service changes

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { IProduct } from './product';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import {catchError, tap } from 'rxjs/operators';
+import {catchError, tap, map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -19,6 +19,12 @@ export class ProductService {
             catchError(this.handleError)
         );
     }
+
+    getProduct(id: number): Observable<IProduct | undefined> {
+        return this.getProducts().pipe(
+          map((products: IProduct[]) => products.find(p => p.productId === id))
+        );
+      }
 
     private handleError(err: HttpErrorResponse) {
         // in read word with send to server remote logging infrastructure instead of logging to console
